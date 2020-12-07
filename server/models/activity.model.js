169d9944e6330ddc57fb991983b43543208d0c77
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-12-07 20:52:09 +0100

    Added styling to activities and explore section. Working on duplicate areas in explore section.

--- server/models/activity.model.js
+++ server/models/activity.model.js
@@ -1,17 +1,22 @@
 const { Schema, model } = require("mongoose");
 
-const activitySchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  area: { type: String, required: true },
-  price: { type: Number, required: true },
-  imageUrl: { type: String, required: true },
-  bookings: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "booking",
-    },
-  ],
-});
+const activitySchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    area: { type: String, required: true },
+    price: { type: Number, required: true },
+    imageUrl: { type: String, required: true },
+    bookings: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "booking",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("Activity", activitySchema);
