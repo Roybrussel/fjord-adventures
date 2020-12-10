364064b81d17bf4085ec78e8095bab7d65940c0d
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-12-10 12:58:33 +0100

    Set index.html file to be renderen on the browser

--- server/app.js
+++ server/app.js
@@ -79,4 +79,15 @@ app.use((req, res, next) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+if (process.env.NODE_ENV === "production") {
+  // set ability to get static values from client build folder
+  // static files include all javascript and css files
+  app.use(express.static("client/build"));
+
+  // get the index.html that will be rendered on the browser
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client", "build", "index.html"));
+  });
+}
+
 module.exports = app;
