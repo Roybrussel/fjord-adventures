f1c255c63972fd7fbafe8060c1d358063a48ce3d
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-05 12:17:26 +0100

    Changed anchor tag Agent Login to Link React Router Dom to act faster.

--- client/src/components/Agentportal/Agentlogin/Loginform/Loginform.js
+++ client/src/components/Agentportal/Agentlogin/Loginform/Loginform.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import AuthService from "../../../../services/auth-service";
 import "./Loginform.css";
 import Form from "react-bootstrap/Form";
